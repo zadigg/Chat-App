9452036278a5465db0420f62897820ca6954c34c
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-11 20:45:50 +0300

    UI

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -17,8 +17,8 @@ function Sidebar() {
   return (
     <>
       <div className=" flex w-full  ">
-        <div className="w-[40%] text-xs md:w-[25%] md:text-base xl:w-[25%] 2xl:w-[20%]  lg:text-base  flex flex-col break-words border-l border border-gray-200 ">
-          <div className="flex justify-between items-center  space-x-3 border-b-2 border-gray-200 break-words">
+        <div className="w-[40%] text-xs md:w-[25%]  md:text-base xl:w-[25%] 2xl:w-[20%]  lg:text-base  flex flex-col break-words border-l border border-gray-200 ">
+          <div className="flex justify-between items-center  space-x-3 border-b-2 border-gray-200 break-words ">
             <div className="px-3 py-4 flex items-center ml-5 ">
               <div>
                 <UserCircleIcon className="w-5 h-5  " />
@@ -68,10 +68,11 @@ function Sidebar() {
           </div>
         </div>
         {/*  */}
-        <div className="w-[75%] xl:w-[75%] 2xl:w-[80%]  ">
-          <div className="px-3 py-4 bg-gray-200 ">
+        <div className="w-[75%] xl:w-[75%] 2xl:w-[80%]   ">
+          {/* <div className="px-3 py-4 bg-gray-200 ">
             <h2 className="font-bold">planck6260@gmail.com</h2>
-          </div>
+          </div> */}
+        
         </div>
       </div>
     </>
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,10 +1,18 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
 
+const Topbar = () => {
+  return (
+    <div className="px-3 py-4 bg-gray-200 wfull ">
+      <h2 className="font-bold">planck6260@gmail.com</h2>
+    </div>
+  );
+};
+
 function Chat() {
   return (
     <>
-      <div className=" h-[100vh]]">
+      <div className="h-[100vh] flex">
         <Sidebar />
       </div>
     </>
