0db72096bbb4e1accf4ba485f2114fdb6256af26
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-11 20:11:48 +0300

    final chat UI

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,33 +1,74 @@
 import React from "react";
-import { LogoutIcon } from "@heroicons/react/outline";
+import { LogoutIcon, UserCircleIcon } from "@heroicons/react/outline";
 
+const Chat = () => {
+  return (
+    <div className="px-3 py-4 flex space-x-2 items-center">
+      <div>
+        <UserCircleIcon className="w-5 h-5 " />
+      </div>
+      <div className="text-xs md:text-md lg:text-lg overflow-hidden">
+        planck6260@gmail.com
+      </div>
+    </div>
+  );
+};
 function Sidebar() {
   return (
     <>
       <div className=" flex w-full h-screen ">
-        <div className="w-[20%]   flex flex-col break-words border-l border border-gray-200 ">
+        <div className="w-[40%] text-xs md:w-[25%] md:text-base xl:w-[25%] 2xl:w-[20%]  lg:text-base  flex flex-col break-words border-l border border-gray-200 ">
           <div className="flex justify-between items-center  space-x-3 border-b-2 border-gray-200 break-words">
-            <div className="px-3 py-4 ">
-              Albert <span className="hidden md:inline-block">Einstein</span>
+            <div className="px-3 py-4 flex items-center ml-5 ">
+              <div>
+                <UserCircleIcon className="w-5 h-5  " />
+              </div>
+              <div className="hidden md:inline text-md">Albert Einstein</div>
             </div>
-            <div>
-              <LogoutIcon className="w-5 h-5 " />
+            <div className="flex justify-end">
+              <LogoutIcon className="w-5 h-5 mr-4" />
             </div>
           </div>
-          <div className="flex flex-col items-center w-full">
+          <div className="flex flex-col w-full">
             <div className="flex justify-center mt-4 w-full">
               <button className="text-xs px-3 py-1 sm:text-sm sm:px-4 md:text-md md:px-10 lg:text-lg lg:px-10 bg-gray-200 rounded-lg font-bold">
                 New Chat
               </button>
             </div>
 
-            <div className="px-3 py-4">
-              <div className="hidden lg:inline-block">planck6260@gmail.com</div>
+            {/*  */}
+            <div className="flex flex-col overflow-y-auto h-[100vh] ml-4 scrollbar-hide ">
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
+              <Chat />
             </div>
+            {/*  */}
           </div>
         </div>
         {/*  */}
-        <div className="w-[80%]">
+        <div className="w-[75%] xl:w-[75%] 2xl:w-[80%] ">
           <div className="px-3 py-4 bg-gray-200 ">
             <h2 className="font-bold">planck6260@gmail.com</h2>
           </div>
