7711e4ed0e3beac600c8fdf427eadd05c46bde12
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-11 20:30:31 +0300

    UI

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,8 @@ import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Sidebar />;
+  return <Component {...pageProps} />;
+
   // <Component {...pageProps} />
 }
 
---
+++ pages/chat/[id].js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../../components/Sidebar";
+
+function Chat() {
+  return (
+    <>
+      <div className=" h-[100vh]]">
+        <Sidebar />
+      </div>
+    </>
+  );
+}
+
+export default Chat;
