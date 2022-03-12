3af9a08eefe6288a8008f8da47fcfab104fac49d
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-12 21:37:11 +0300

    UI

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,7 +9,10 @@ import { collection } from "@firebase/firestore";
 import { db } from "../firebaseconfig";
 import { getFirestore } from "firebase/firestore";
 
-Hello
+// Hellogit add . 
+git commit -m "UI" 
+git add .
+git push -u origin main
 function Sidebar() {
   const [user] = useAuthState(auth);
   const [snapshot, loading, error] = useCollection(collection(db, "chats"));
