0bd197b5e42538716cd6faf5425bdd361fbcd529
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-12 15:23:43 +0300

    UI

---
+++ firebaseconfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLNwlyjcnOQxLD7vaV-nePBYCj5XGLk8M",
+  authDomain: "chatapp-abcbc.firebaseapp.com",
+  projectId: "chatapp-abcbc",
+  storageBucket: "chatapp-abcbc.appspot.com",
+  messagingSenderId: "109047449236",
+  appId: "1:109047449236:web:fec7947683781bb7729681",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export { auth };
