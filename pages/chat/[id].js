4ffc0facbad74e98e050d467404f6c7b560b3814
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-12 14:02:47 +0300

    UI

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,7 +1,35 @@
+import { Flex } from "@chakra-ui/react";
 import React from "react";
-
+import Sidebar from "../../components/Sidebar";
+import Head from "next/head"
 function Chat() {
-  return <div>Chat</div>;
+  return (
+    <Flex h="100vh">
+      <Head>
+        <title>Chat App</title>
+      </Head>
+
+      <Sidebar />
+
+      <Flex flex={1} direction="column">
+        <Topbar email={getOtherEmail(chat?.users, user)} />
+
+        <Flex
+          flex={1}
+          direction="column"
+          pt={4}
+          mx={5}
+          overflowX="scroll"
+          sx={{ scrollbarWidth: "none" }}
+        >
+          {getMessages()}
+          <div ref={bottomOfChat}></div>
+        </Flex>
+
+        <Bottombar id={id} user={user} />
+      </Flex>
+    </Flex>
+  );
 }
 
 export default Chat;
