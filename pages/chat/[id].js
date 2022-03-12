0e7bdbd9a38e6dbece5ef7b2324982fbdadaf731
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-12 14:27:38 +0300

    UI

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,13 +1,33 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
 import Head from "next/head";
-import { Avatar, Flex } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Flex,
+  FormControl,
+  Heading,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 
 const Topbar = () => {
   return (
-    <Flex bg="gray.100" h="81px" w="100%" align="center">
-      <Avatar />
+    <Flex bg="gray.100" h="81px" w="100%" align="center" p={5}>
+      <Avatar src="" marginEnd={3} />
+      <Heading size="lg"> user@gmail.com</Heading>
     </Flex>
+  );
+};
+
+const Bottombar = () => {
+  return (
+    <FormControl p={3}>
+      <Input placeholder="Type a message" />
+      <Button type="submit" hidden autoComplete="off">
+        Submit
+      </Button>
+    </FormControl>
   );
 };
 
@@ -23,18 +43,40 @@ function Chat() {
       <Flex flex={1} direction="column">
         <Topbar />
 
-        <Flex
-          flex={1}
-          direction="column"
-          pt={4}
-          mx={5}
-          overflowX="scroll"
-          sx={{ scrollbarWidth: "none" }}
-        >
-          {/* <div ref={bottomOfChat}></div> */}
+        <Flex flex={1} direction="column">
+          <Flex
+            bg="blue.100"
+            w="fit-content"
+            minWidth="100px"
+            borderRadius="lg"
+            p={3}
+            m={1}
+          >
+            <Text>This is a dummy message</Text> m={1}
+          </Flex>{" "}
+          <Flex
+            bg="blue.100"
+            w="fit-content"
+            minWidth="100px"
+            borderRadius="lg"
+            p={3}
+            m={1}
+          >
+            <Text>This is a dummy message</Text>
+          </Flex>
+          <Flex
+            bg="blue.100"
+            w="fit-content"
+            minWidth="100px"
+            borderRadius="lg"
+            p={3}
+            m={1}
+          >
+            <Text>This is a dummy message</Text>
+          </Flex>
         </Flex>
 
-        {/* <Bottombar id={id} user={user} /> */}
+        <Bottombar />
       </Flex>
     </Flex>
   );
