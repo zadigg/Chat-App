1a99fb3eab2f93414db737268e7e7de842a0f0cd
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-12 15:31:24 +0300

    UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Spinner, Center } from "@chakra-ui/react";
 import Login from "../components/Login";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebaseconfig";
 function MyApp({ Component, pageProps }) {
-  const [user, loading, error] = useAuthState(auth, options);
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <ChakraProvider>
+        <Center h="100vh">
+          <Spinner size="xl" />
+        </Center>
+      </ChakraProvider>
+    );
+  }
+
+  if (!user) {
+    return (
+      <ChakraProvider>
+        <Login />
+      </ChakraProvider>
+    );
+  }
   return (
     <ChakraProvider>
       <Component {...pageProps} />
