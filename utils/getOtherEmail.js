7a04775d063c168eb379c9de8e72e3871f1f73a8
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-13 13:47:23 +0300

    UI

--- utils/getOtherEmail.js
+++ utils/getOtherEmail.js
@@ -1,7 +1,5 @@
-import React from "react";
-
-function getOtherEmail(users, currentUser) {
-  return users.filter((user) => user !== currentUser)[0];
-}
+const getOtherEmail = (users, currentUser) => {
+  return users?.filter((user) => user !== currentUser.email)[0];
+};
 
 export default getOtherEmail;
