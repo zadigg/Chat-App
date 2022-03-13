dba7cf800c14cef321fc519f7dae5bc3f156c174
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-13 13:46:52 +0300

    UI

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,33 +1,54 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { Button } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/button";
+import { Flex, Text } from "@chakra-ui/layout";
 import { ArrowLeftIcon } from "@chakra-ui/icons";
-import { Avatar, Button, Flex, IconButton, Text } from "@chakra-ui/react";
-import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseconfig";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { collection } from "@firebase/firestore";
+import { collection, addDoc } from "@firebase/firestore";
 import { db } from "../firebaseconfig";
-import { getFirestore } from "firebase/firestore";
 import getOtherEmail from "../utils/getOtherEmail";
+import { useRouter } from "next/router";
 
-function Sidebar() {
+export default function Sidebar() {
   const [user] = useAuthState(auth);
   const [snapshot, loading, error] = useCollection(collection(db, "chats"));
   const chats = snapshot?.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  const router = useRouter();
+
+  const redirect = (id) => {
+    router.push(`/chat/${id}`);
+  };
+
+  const chatExists = (email) =>
+    chats?.find(
+      (chat) => chat.users.includes(user.email) && chat.users.includes(email)
+    );
+
+  const newChat = async () => {
+    const input = prompt("Enter email of chat recipient");
+    if (!chatExists(input) && input != user.email) {
+      await addDoc(collection(db, "chats"), { users: [user.email, input] });
+    }
+  };
 
   const chatList = () => {
-    return chats?.map((chat) => (
-      <Flex
-        key={Math.random()}
-        p={3}
-        align="center"
-        _hover={{ bg: "gray.100", cursor: "pointer" }}
-        onClick={() => redirect(chat.id)}
-      >
-        <Avatar src="" marginEnd={3} />
-        <Text>{getOtherEmail(chat.users, user)}</Text>
-      </Flex>
-    ));
+    return chats
+      ?.filter((chat) => chat.users.includes(user.email))
+      .map((chat) => (
+        <Flex
+          key={Math.random()}
+          p={3}
+          align="center"
+          _hover={{ bg: "gray.100", cursor: "pointer" }}
+          onClick={() => redirect(chat.id)}
+        >
+          <Avatar src="" marginEnd={3} />
+          <Text>{getOtherEmail(chat.users, user)}</Text>
+        </Flex>
+      ));
   };
 
   return (
@@ -60,14 +81,16 @@ function Sidebar() {
           icon={<ArrowLeftIcon />}
           onClick={() => signOut(auth)}
         />
+        
       </Flex>
 
-      <Button m={5} p={4}>
+      <Button m={5} p={4} onClick={() => newChat()}>
         New Chat
       </Button>
+
       <Flex
-        direction="column"
         overflowX="scroll"
+        direction="column"
         sx={{ scrollbarWidth: "none" }}
         flex={1}
       >
@@ -76,5 +99,3 @@ function Sidebar() {
     </Flex>
   );
 }
-
-export default Sidebar;
